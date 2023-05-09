c27fa19c7498485263985f92e50d169cd70974d5
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-09 07:04:14 +0000

    [Start]JavaScript

---
+++ app.js
@@ -0,0 +1,50 @@
+// let hello='Hello World';
+// alert(hello);
+
+// let int1 = 1;
+// alert(int1);
+
+// let int2 = -10;
+// alert(int2);
+
+// let float1 = 3.14;
+// alert(float1);
+
+// let str1 = 'JavaScriptを覚えよう';
+// alert(str1);
+
+// let str1='Hello';
+// let str2=' ';
+// let str3='World!!';
+// alert(str1+str2+str3);
+
+// let orange = 100;
+// let apple = 100;
+
+// if(orange < apple){
+  // alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+  // alert('みかんとりんごが同じ値段');
+// } else{
+  // alert('みかんの値段がりんごより高い');
+// }
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+  // num = num*5;
+  // count = count+1;
+// }
+
+// alert('5をかけ続けて'+ max +'を越えるのに必要だった回数は'+ count +'回です');
+
+let i;
+let num = 0;
+
+for(i = 1;i < 11;i++){
+  num = num + i;
+}
+
+alert('1から１０まで足し算した結果は'+ num +'です');
