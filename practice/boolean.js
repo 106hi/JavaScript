24eb4d79c6c37df3bf71dcc236e4fe6a65408e95
106hi <hiro3428shs96kzsi@gmail.com>
2023-08-10 08:52:47 +0000

    Add boolean.js

---
+++ practice/boolean.js
@@ -0,0 +1,11 @@
+'use strict';
+var value1 = 0;
+var boolean1 = Boolean(value1);
+console.log(boolean1);
+
+var value2 = 1;
+var boolean2 = Boolean(value2);
+console.log(boolean2);
+
+console.log(boolean1 === false);
+console.log(boolean2 === true);
