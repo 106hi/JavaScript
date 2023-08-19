01f2a9d68f20e13783f9bb165c2ffc16dfd80f09
106hi <hiro3428shs96kzsi@gmail.com>
2023-08-19 07:11:36 +0000

    Fix run.js

--- practice/run.js
+++ practice/run.js
@@ -1,12 +1,15 @@
 const fuel = 100;
 const model = "X-1976";
+const customPropertyName = "shippedBy";
 const car = {
   fuel,
   model,
   run() {
     console.log("Run");
-  }
+  },
+  [customPropertyName]: "1977"
 };
 console.log(car.fuel);
 console.log(car.model);
+console.log(car.shippedBy);
 car.run();
